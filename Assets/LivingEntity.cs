a1bb4612b255aaba09c9f4f1d405d85df58b89a8
rraftery-1342715 <72582562+rraftery-1342715@users.noreply.github.com>
2022-03-13 00:55:38 -0800

    Added some enemy scripts
    Added the LivingEntity script, IDamagable interface, a base class for the enemy called EnemyBase, and a basic patrol AI called PlatformPatrollerEnemy

---
+++ Assets/LivingEntity.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamagable
+{
+    public float health {get; protected set;}
+    public float maxHealth = 3;
+    public GameObject deathObject;
+    protected bool isDead = false;
+    public event System.Action OnDeath;
+    protected virtual void Awake(){
+        health = maxHealth;
+    }
+
+    public virtual void TakeDamage(float damage){
+        health -= damage;
+        if(health <= 0 && !isDead){
+            Die();
+        }
+        
+    }
+
+    public virtual void Die(){
+        //CameraScript.instance.Shake(deathShakeDuration, deathShakeMagnitude);
+        
+        isDead = true;
+        if(deathObject != null){
+            Destroy(Instantiate(deathObject, transform.position, Quaternion.identity), 1f);
+        }
+        if(OnDeath != null){
+            OnDeath();
+        }
+        Destroy(gameObject);
+    }
+
+    protected virtual bool CheckOnDeathNull(){
+        return OnDeath == null;
+    }
+
+    protected virtual void CallOnDeath(){
+        if(OnDeath != null){
+            OnDeath();
+        }
+        
+    }
+}
